64373e859fd0a736295f687ba5fb19c50d92d0c4
soymou <emilio.junoy@gmail.com>
2026-01-08 20:53:17 -0600

    Style

--- dots/.config/fish/config.fish
+++ dots/.config/fish/config.fish
@@ -1,3 +1,5 @@
+set -U fish_greeting "Hack the web"
+
 function fish_prompt -d "Write out the prompt"
     # This shows up as USER@HOST /home/user/ >, with the directory colored
     # $USER and $hostname are set by fish, so you can just use them
